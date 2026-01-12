529f78ba9b01241268825d5b1222182a8d6d2ce8
Lam Ba Le Thuong <36751158+thuonglbl@users.noreply.github.com>
2026-01-12 16:20:23 +0700

    Integrate Azure AI agent for expense processing

--- Labfiles/04-agent-framework/python/agent-framework.py
+++ Labfiles/04-agent-framework/python/agent-framework.py
@@ -3,14 +3,18 @@ import asyncio
 from pathlib import Path
 
 # Add references
-
+from agent_framework import AgentThread, ChatAgent
+from agent_framework.azure import AzureAIAgentClient
+from azure.identity.aio import AzureCliCredential
+from pydantic import Field
+from typing import Annotated
 
 
 async def main():
     # Clear the console
     os.system('cls' if os.name=='nt' else 'clear')
 
-    # Load the expnses data file
+    # Load the expenses data file
     script_dir = Path(__file__).parent
     file_path = script_dir / 'data.txt'
     with file_path.open('r') as file:
@@ -25,14 +29,39 @@ async def main():
 async def process_expenses_data(prompt, expenses_data):
 
     # Create a chat agent
-    
+    async with (
+        AzureCliCredential() as credential,
+        ChatAgent(
+            chat_client=AzureAIAgentClient(async_credential=credential),
+            name="expenses_agent",
+            instructions="""You are an AI assistant for expense claim submission.
+                        When a user submits expenses data and requests an expense claim, use the plug-in function to send an email to expenses@contoso.com with the subject 'Expense Claim`and a body that contains itemized expenses with a total.
+                        Then confirm to the user that you've done so.""",
+            tools=send_email,
+        ) as agent,
+    ):    
 
         # Use the agent to process the expenses data    
-
+        try:
+            # Add the input prompt to a list of messages to be submitted
+            prompt_messages = [f"{prompt}: {expenses_data}"]
+            # Invoke the agent for the specified thread with the messages
+            response = await agent.run(prompt_messages)
+            # Display the response
+            print(f"\n# Agent:\n{response}")
+        except Exception as e:
+            # Something went wrong
+            print (e)
 
 
 # Create a tool function for the email functionality
-
+def send_email(
+ to: Annotated[str, Field(description="Who to send the email to")],
+ subject: Annotated[str, Field(description="The subject of the email.")],
+ body: Annotated[str, Field(description="The text body of the email.")]):
+     print("\nTo:", to)
+     print("Subject:", subject)
+     print(body, "\n")
 
 
 if __name__ == "__main__":
